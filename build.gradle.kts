f27a8ab45ea76497dc5588da76ce796a1f45c8fa
D4C4N <dacan1508@gmail.com>
2025-01-17 17:47:37 +0100

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,36 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.1.0"
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("com.google.inject:guice:7.0.0")
+    implementation("com.google.guava:guava:33.4.0-jre")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.mockito:mockito-core:5.15.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    testLogging {
+        events("PASSED", "FAILED", "SKIPPED")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
